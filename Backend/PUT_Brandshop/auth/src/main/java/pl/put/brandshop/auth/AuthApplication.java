5fd07723fed1601399a8e12fcedfeb82e0a9e3ed
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-11-18 11:37:59 +0100

    PUTBrandshopPO -> Backend -> Auth Service -> Init

--- Backend/PUT_Brandshop/auth/src/main/java/pl/put/brandshop/auth/AuthApplication.java
+++ Backend/PUT_Brandshop/auth/src/main/java/pl/put/brandshop/auth/AuthApplication.java
@@ -2,8 +2,12 @@ package pl.put.brandshop.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
 
 	public static void main(String[] args) {
